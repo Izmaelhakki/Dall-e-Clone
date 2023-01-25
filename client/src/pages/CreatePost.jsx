3573b5e277910d72b1a11e448fbe2a3a20cd12c1
Izmaelhakki <100159172+Izmaelhakki@users.noreply.github.com>
2023-01-25 16:35:52 +0300

    Create Image and My Api

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -17,7 +17,31 @@ function CreatePost() {
 
   const [loading, setloading] = useState(false);
 
-  const generateImage = () => {};
+  const generateImage = async() => {
+    if(form.prompt){
+      try {
+        setGeneratingImg(true);
+        const  response= await fetch('http://localhost:8080/api/v1/dalle',{
+          method:'POST',
+          headers:{
+            'Content-Type':'application/json',
+          },
+          body: JSON.stringify({prompt:form.prompt}),
+       })
+
+       const data=await response.json();
+       console.log(data);
+       setForm({...form,photo:`data:image/jpeg;base64,${data.photo}`})
+      } catch (error) {
+        alert(error);
+      }finally{
+        setGeneratingImg(false);
+      }
+    }else{
+      console.log('Please enter a prompt');
+    }
+
+  };
 
   const handleSubmit = () => {};
 
